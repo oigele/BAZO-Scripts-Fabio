0f7c37530ed2d6495e0a1259d4b5860f37a648cb
febe19 <fabio.maddaloni@uzh.ch>
2018-11-17 13:20:45 +0100

    download root miner

---
+++ RootMiner_setup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Automated script for downloading and preparing the Root Miner for Bazo-blockchain"
+echo "  Downloading BAZO-Miner from Github"
+go get github.com/bazo-blockchain/bazo-miner
+cd /home/ubuntu/go/src/github.com/bazo-blockchain/bazo-miner
+go build
+echo "Download & build of BAZO-Miner - Finished"
+echo " "
