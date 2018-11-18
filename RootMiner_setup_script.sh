afb564b70a109eedb93b5018d7bc5e108ecc42ea
febe19 <fabio.maddaloni@uzh.ch>
2018-11-18 15:01:02 +0100

    Remove pubIP

--- RootMiner_setup_script.sh
+++ RootMiner_setup_script.sh
@@ -7,10 +7,3 @@ cd /home/ubuntu/go/src/github.com/bazo-blockchain/bazo-miner
 go build
 echo "Download & build of BAZO-Miner - Finished"
 echo " "
-
-echo "This machines public IP-adress --> Alias: pubIP"
-curl https://ipinfo.io/ip
-echo "alias pubIP='curl https://ipinfo.io/ip'" >> ~/.bashrc
-source ~/.bashrc
-cd
-source .bashrc
