b6f802f55caaa7e8dec49a2310fe3fa64411e45e
febe19 <fabio.maddaloni@uzh.ch>
2018-11-21 09:23:09 +0100

    checkout aduno cherry pick

--- RootMiner_setup_script.sh
+++ RootMiner_setup_script.sh
@@ -4,6 +4,7 @@ echo "Automated script for downloading and preparing the Root Miner for Bazo-blo
 echo "  Downloading BAZO-Miner from Github"
 go get github.com/bazo-blockchain/bazo-miner
 cd /home/ubuntu/go/src/github.com/bazo-blockchain/bazo-miner
+git checkout origin/aduno_copy-cherry-picks
 go build
 echo "Download & build of BAZO-Miner - Finished"
 echo " "
