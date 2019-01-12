551d8f22a3b5ec196c7f1e43dd826951da2d73f6
febe19 <fabio.maddaloni@uzh.ch>
2019-01-12 19:05:05 +0100

    make new space

--- testScript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pbIP=$(curl https://ipinfo.io/ip)
-btIP=123.123.123.123
-echo " "
-echo "./bazo-miner start --database Store.db --address $pbIP:8000 --bootstrap $btIP:8000 --wallet Wallet.txt --commitment Commitment.txt --multisig RootAccountWallet.txt --rootwallet RootAccountWallet.txt --rootcommitment RootAccountCommitment.txt --confirm" 
