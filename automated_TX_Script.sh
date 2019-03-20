421f1d0b4c24f1d53f5690f0df1d574e0a4eef42
febe19 <fabio.maddaloni@uzh.ch>
2019-03-20 13:15:21 +0100

    Typo

--- automated_TX_Script.sh
+++ automated_TX_Script.sh
@@ -2,43 +2,43 @@
 
 for i in {0..10000}
 do
-#	bazo-client funds --from Wallets/Wallets/WalletGCPOregon.txt --to Wallets/Wallets/WalletGCPSouthCarolina.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPOregon.txt --to Wallets/Wallets/WalletGCPSouthCarolina.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/Wallets/WalletGCPSouthCarolina.txt --to Wallets/Wallets/WalletGCPIowa.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/Wallets/WalletGCPSouthCarolina.txt --to Wallets/Wallets/WalletGCPIowa.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPIowa.txt --to Wallets/Wallets/WalletGCPSaoPaulo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPIowa.txt --to Wallets/Wallets/WalletGCPSaoPaulo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPSaoPaulo.txt --to Wallets/Wallets/WalletGCPFrankfurt.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPSaoPaulo.txt --to Wallets/Wallets/WalletGCPFrankfurt.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPFrankfurt.txt --to Wallets/Wallets/WalletGCPFinland.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPFrankfurt.txt --to Wallets/Wallets/WalletGCPFinland.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPFinland.txt --to Wallets/Wallets/WalletGCPNetherlands.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPFinland.txt --to Wallets/Wallets/WalletGCPNetherlands.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPNetherlands.txt --to Wallets/Wallets/WalletGCPSydney.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPNetherlands.txt --to Wallets/Wallets/WalletGCPSydney.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPSydney.txt --to Wallets/Wallets/WalletGCPHongKong.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPSydney.txt --to Wallets/Wallets/WalletGCPHongKong.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPHongKong.txt --to Wallets/Wallets/WalletGCPTokyo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPHongKong.txt --to Wallets/Wallets/WalletGCPTokyo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPTokyo.txt --to Wallets/Wallets/WalletGCPTaiwan.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPTokyo.txt --to Wallets/Wallets/WalletGCPTaiwan.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPTaiwan.txt --to Wallets/Wallets/WalletGCPLosAngeles.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPTaiwan.txt --to Wallets/Wallets/WalletGCPLosAngeles.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletGCPLosAngeles.txt --to Wallets/Wallets/WalletAWSLondon.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletGCPLosAngeles.txt --to Wallets/Wallets/WalletAWSLondon.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSLondon.txt --to Wallets/Wallets/WalletAWSParis.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSLondon.txt --to Wallets/Wallets/WalletAWSParis.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSParis.txt --to Wallets/Wallets/WalletAWSSaoPaulo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSParis.txt --to Wallets/Wallets/WalletAWSSaoPaulo.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSSaoPaulo.txt --to Wallets/Wallets/WalletAWSSeoul.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSSaoPaulo.txt --to Wallets/Wallets/WalletAWSSeoul.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSSeoul.txt --to Wallets/Wallets/WalletAWSMumbai.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSSeoul.txt --to Wallets/Wallets/WalletAWSMumbai.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSMumbai.txt --to Wallets/Wallets/WalletAWSSingapore.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSMumbai.txt --to Wallets/Wallets/WalletAWSSingapore.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSSingapore.txt --to Wallets/Wallets/WalletAWSMontreal.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSSingapore.txt --to Wallets/Wallets/WalletAWSMontreal.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 
-#	bazo-client funds --from Wallets/Wallets/WalletAWSMontreal.txt --to Wallets/Wallets/WalletGCPOregon.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
+#	./bazo-client funds --from Wallets/Wallets/WalletAWSMontreal.txt --to Wallets/Wallets/WalletGCPOregon.txt --txcount $i --amount 1 --multisig Wallets/WalletB04Root.txt
 	sleep 0.1
 done
 
