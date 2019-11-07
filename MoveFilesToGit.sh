4ac63add37caf7ca37ae09f3aaa95994a6401315
Raphael Beckmann <raphael.beckmann@uzh.ch>
2019-11-07 13:26:33 +0100

    Update MoveFilesToGit.sh

--- MoveFilesToGit.sh
+++ MoveFilesToGit.sh
@@ -2,18 +2,18 @@
 
 echo " "
 echo "Deleting old wallets and commitments"
-cd ~/BAZO/Scripts-Fabio/Wallets
+cd ~/BAZO-Scripts-Fabio/Wallets
 rm *
 echo "Deleted all wallets"
-cd ~/BAZO/Scripts-Fabio/Commitments
+cd ~/BAZO-Scripts-Fabio/Commitments
 rm *
 echo "Deleted all commitments"
 echo "Copying wallets to git repository"
 cd ~/Bazo/bin
-cp wallet.txt wallet1.txt ~/BAZO/Scripts-Fabio/Wallets
+cp wallet.txt wallet1.txt ~/BAZO-Scripts-Fabio/Wallets
 echo "Copying commitments to git repository"
 cp commitment.txt commitment1.txt ~/BAZO-Scripts-Fabio/Commitments
 echo "Stage files"
-cd ~/BAZO/Scripts-Fabio/
+cd ~/BAZO-Scripts-Fabio/
 git add *
 echo "End of this program. Now the files need to be commited and pushed"
