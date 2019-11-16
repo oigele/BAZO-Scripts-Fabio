44a3f65891df46c2e0026643147c713d5dafad57
oigele <raphael.beckmann@uzh.ch>
2019-11-16 21:50:29 +0000

    mod

--- DeleteAllGeneratedFiles.sh
+++ DeleteAllGeneratedFiles.sh
@@ -5,14 +5,12 @@ cd ~/Bazo/src/github.com/oigele/bazo-miner
 echo "Removing all files from miner"
 rm Log*
 rm Store*
-rm wallet*
-rm commitment*
+rm GCP*
 cd ~/Bazo/src/github.com/oigele/bazo-client
 echo "Removing all files from client"
 rm Log*
 rm Store*
-rm wallet*
-rm commitment*
+rm GCP*
 rm client.db
 rm Performance*
 echo "script execution finished. All data deleted"
