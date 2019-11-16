104f5c5faa502e159a4efd0915ed26b88a85c8ba
oigele <raphael.beckmann@uzh.ch>
2019-11-16 18:56:31 +0000

    Modified AddAccounts

--- AddAccountsScript.sh
+++ AddAccountsScript.sh
@@ -15,49 +15,18 @@ fi
 
 mkdir Commitments
 
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPOregon.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPSouthCarolina.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPIowa.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPSaoPaulo.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPFrankfurt.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPFinland.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPNetherlands.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPSydney.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPHongKong.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPTokyo.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPTaiwan.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletGCPLosAngeles.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSLondon.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSParis.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSSaoPaulo.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSSeoul.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSMumbai.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSSingapore.txt
-./bazo-client account create --rootwallet Wallets/WalletB04Root.txt --wallet Wallets/WalletAWSMontreal.txt
+./bazo-client account create --rootwallet Wallets/WalletGCPCentral.txt --wallet Wallets/WalletGCPEast.txt
+./bazo-client account create --rootwallet Wallets/WalletGCPCentral.txt --wallet Wallets/WalletGCPWest.txt
+./bazo-client account create --rootwallet Wallets/WalletGCPCentral.txt --wallet Wallets/WalletGCPNortheast.txt
+./bazo-client account create --rootwallet Wallets/WalletGCPCentral.txt --wallet Wallets/WalletGCPEuropenorth.txt
 
 
 echo " "
 echo "Added GCP accounts for:"
-echo " - Oregon"
-echo " - SoutCarolina" 
-echo " - Iowa"
-echo " - SaoPaulo"
-echo " - Frankfurt"
-echo " - Finland"
-echo " - Netherlands"
-echo " - Sydney"
-echo " - HongKong"
-echo " - Tokyo"
-echo " - Taiwan"
-echo " - LosAngeles"
-echo "Added AWS Accounts for:"
-echo " - London"
-echo " - Paris"
-echo " - SaoPaulo"
-echo " - Seoul"
-echo " - Mumbai"
-echo " - Singapore"
-echo " - Montreal"
+echo " - East"
+echo " - West" 
+echo " - Northeast"
+echo " - Europenorth"
 echo " "
 echo "--------------------------------------------------------------------------------------"
 echo "Check if all Accounts are added to the chain and are visible in the state."
@@ -69,25 +38,10 @@ if [ "$selection" = "y" ]
 	else exit 1 
 fi
 echo " "
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPOregon.txt --txcount 0 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPSouthCarolina.txt --txcount 1 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPIowa.txt --txcount 2 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPSaoPaulo.txt --txcount 3 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPFrankfurt.txt --txcount 4 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPFinland.txt --txcount 5 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPNetherlands.txt --txcount 6 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPSydney.txt --txcount 7 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPHongKong.txt --txcount 8 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPTokyo.txt --txcount 9 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPTaiwan.txt --txcount 10 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletGCPLosAngeles.txt --txcount 11 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSLondon.txt --txcount 12 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSParis.txt --txcount 13 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSSaoPaulo.txt --txcount 14 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSSeoul.txt --txcount 15 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSMumbai.txt --txcount 16 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSSingapore.txt --txcount 17 --amount 15000 --multisig Wallets/WalletB04Root.txt
-./bazo-client funds --from Wallets/WalletB04Root.txt --to Wallets/WalletAWSMontreal.txt --txcount 18 --amount 15000 --multisig Wallets/WalletB04Root.txt
+./bazo-client funds --from Wallets/WalletGCPCentral.txt --to Wallets/WalletGCPEast.txt --txcount 0 --amount 15000 --multisig Wallets/WalletGCPCentral.txt
+./bazo-client funds --from Wallets/WalleGCPCentral.txt --to Wallets/WalletGCPWest.txt --txcount 1 --amount 15000 --multisig Wallets/WalletGCPCentral.txt
+./bazo-client funds --from Wallets/WalletGCPCentral.txt --to Wallets/WalletGCPNortheast.txt --txcount 2 --amount 15000 --multisig Wallets/WalletGCPCentral.txt
+./bazo-client funds --from Wallets/WalletGCPCentral.txt --to Wallets/WalletGCPEuropenorth.txt --txcount 3 --amount 15000 --multisig Wallets/WalletGCPCentral.txt
 
 echo " "
 echo "Sent coins to the previously created Accounts."
@@ -101,25 +55,10 @@ if [ "$selection" = "y" ]
 	else exit 1 
 fi
 
-./bazo-client staking enable --wallet Wallets/WalletGCPOregon.txt --commitment Commitments/CommitmentGCPOregon.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPSouthCarolina.txt --commitment Commitments/CommitmentGCPSouthCarolina.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPIowa.txt --commitment Commitments/CommitmentGCPIowa.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPSaoPaulo.txt --commitment Commitments/CommitmentGCPSaoPaulo.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPFrankfurt.txt --commitment Commitments/CommitmentGCPFrankfurt.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPFinland.txt --commitment Commitments/CommitmentGCPFinland.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPNetherlands.txt --commitment Commitments/CommitmentGCPNetherlands.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPSydney.txt --commitment Commitments/CommitmentGCPSydney.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPHongKong.txt --commitment Commitments/CommitmentGCPHongKong.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPTokyo.txt --commitment Commitments/CommitmentGCPTokyo.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPTaiwan.txt --commitment Commitments/CommitmentGCPTaiwan.txt
-./bazo-client staking enable --wallet Wallets/WalletGCPLosAngeles.txt --commitment Commitments/CommitmentGCPLosAngeles.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSLondon.txt --commitment Commitments/CommitmentAWSLondon.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSParis.txt --commitment Commitments/CommitmentAWSParis.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSSaoPaulo.txt --commitment Commitments/CommitmentAWSSaoPaulo.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSSeoul.txt --commitment Commitments/CommitmentAWSSeoul.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSMumbai.txt --commitment Commitments/CommitmentAWSMumbai.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSSingapore.txt --commitment Commitments/CommitmentAWSSingapore.txt
-./bazo-client staking enable --wallet Wallets/WalletAWSMontreal.txt --commitment Commitments/CommitmentAWSMontreal.txt
+./bazo-client staking enable --wallet Wallets/WalletGCPEast.txt --commitment Commitments/CommitmentGCPEast.txt
+./bazo-client staking enable --wallet Wallets/WalletGCPWest.txt --commitment Commitments/CommitmentGCPWest.txt
+./bazo-client staking enable --wallet Wallets/WalletGCPNortheast.txt --commitment Commitments/CommitmentGCPNortheast.txt
+./bazo-client staking enable --wallet Wallets/WalletGCPEuropenorth.txt --commitment Commitments/CommitmentGCPEuropenorth.txt
 
 echo " "
 echo "Check if Staking is enabled for all accounts. If so, distribute the Wallets & Commitments to all miners & clients and start them."
---
+++ automatedTx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/Bazo/src/github.com/oigele/bazo-client
+
+for i in {0..10000}
+do
+	./bazo-client funds --from wallet.txt --to wallet1.txt --txcount $i --amount 20 --multisig wallet.txt
+
+
+cd ~/Bazo/src/github.com/oigele/bazo-client
+sleep 0.1
+done
