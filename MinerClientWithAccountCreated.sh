e312982484642b7a791fb8ca3bd85d966dbacd68
oigele <raphael.beckmann@uzh.ch>
2019-11-17 14:06:51 +0000

    bugfix

--- MinerClientWithAccountCreated.sh
+++ MinerClientWithAccountCreated.sh
@@ -6,7 +6,7 @@ echo "Plese insert the name of the Location where you set up this VM:"
 read location
 echo "We are now setting up a VM in Location $location running on GCP"
 
-cd ~/go/src/github.com/bazo-blockchain/bazo-client
+cd ~/Bazo/src/github.com/oigele/bazo-client
 pbIP=$(curl https://ipinfo.io/ip)
 echo " "
 echo "This machines public IP:"
@@ -15,14 +15,14 @@ echo "enter bootstrap IP: [number]"
 read btIP
 jq '.this_client.ip="'$pbIP'"' configuration.json > temp.json
 rm configuration.json
-jq '.bootstrap_server.ip="'$pbIP'"' temp.json > temp2.json
-jq '.bootstrap_server.port="'8001'"' temp2.json > temp.json
+jq '.bootstrap_server.ip="'$btIP'"' temp.json > temp2.json
+jq '.bootstrap_server.port="'8000'"' temp2.json > temp.json
 jq '.multisig_server.ip="'$btIP'"' temp.json > configuration.json
 echo "configuration.json"
 cat configuration.json
 rm temp.json
 rm temp2.json
-cd ~/go/src/github.com/bazo-blockchain/bazo-client
+cd ~/Bazo/src/github.com/oigele/bazo-client
 
 cd ~/BAZO-Scripts-Fabio
 git pull
@@ -34,8 +34,8 @@ echo " "
 echo "Is there a Wallet with the Wallet GCP$location.txt on the list? [y/n]"
 read selection
 if [ "$selection" = "y" ]
-  then cp -r ~/BAZO-Scripts-Fabio/Wallets ~/go/src/github.com/bazo-blockchain/bazo-miner
-       cp -r ~/BAZO-Scripts-Fabio/Wallets ~/go/src/github.com/bazo-blockchain/bazo-client
+  then cp -r ~/BAZO-Scripts-Fabio/Wallets/* ~/Bazo/src/github.com/oigele/bazo-miner
+       cp -r ~/BAZO-Scripts-Fabio/Wallets/* ~/Bazo/src/github.com/oigele/bazo-client
        echo "Wallet folder is successfully copied to the miner and client"
   else git pull origin
        echo "copy Wallet folder manually to the client and miner folder"
@@ -49,7 +49,7 @@ echo " "
 echo "Is there a Commitment with the name Commitment$provider$location.txt on the list above? [y/n]"
 read selection
 if [ "$selection" = "y" ]
-  then cp -r ~/BAZO-Scripts-Fabio/Commitments ~/go/src/github.com/bazo-blockchain/bazo-miner
+  then cp -r ~/BAZO-Scripts-Fabio/Commitments/* ~/Bazo/src/github.com/oigele/bazo-miner
        echo "Commitment folder is successfully copied to the miner."
   else git pull origin
        echo "Copy Commitment folder manually to the miner folder"
@@ -57,5 +57,5 @@ fi
 
 echo " "
 echo "Bazo Miner can be started in the Miner folder with: "
-echo "./bazo-miner start --database Store.db --address $pbIP:8001 --bootstrap $btIP:8000 --wallet Wallets/WalletGCP$location.txt --commitment Commitments/CommitmentGCP$location.txt --multisig Wallets/WalletGCPCentral.txt --rootwallet Wallets/WalletGCPCentral.txt --rootcommitment Commitments/CommitmentGCPCentral.txt --confirm"
+echo "./bazo-miner start --database Store.db --address $pbIP:8001 --bootstrap $btIP:8000 --wallet WalletGCP$location.txt --commitment CommitmentGCP$location.txt --multisig WalletGCPCentral.txt --rootwallet WalletGCPCentral.txt --rootcommitment CommitmentGCPCentral.txt --confirm"
 echo " "
