ab06d92440a83464fe4557b57a981a4a49bab0b7
oigele <raphael.beckmann@uzh.ch>
2020-01-05 17:48:29 +0000

    2 new miners

--- automatedTx.sh
+++ automatedTx.sh
@@ -4,9 +4,7 @@ cd ~/Bazo/src/github.com/oigele/bazo-client
 
 for i in {0..10000}
 do
-	./bazo-client funds --from WalletGCPCentral.txt --to wallet1.txt --txcount $i --amount 20 --multisig wallet.txt
+	./bazo-client funds --from WalletGCPCentral.txt --to WalletGCPEast.txt --txcount $i --amount 20 --multisig wallet.txt
 
 
-cd ~/Bazo/src/github.com/oigele/bazo-client
-sleep 0.1
 done
