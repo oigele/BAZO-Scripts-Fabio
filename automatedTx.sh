24b1ef3a52d6392028dfdf37af224d3fb47064cc
oigele <raphael.beckmann@uzh.ch>
2020-01-22 12:58:24 +0000

    second miner

--- automatedTx.sh
+++ automatedTx.sh
@@ -4,7 +4,7 @@ cd ~/Bazo/src/github.com/oigele/bazo-client
 
 for i in {0..5000}
 do
-	./bazo-client funds --from WalletGCPCentral.txt --to WalletGCPEast.txt --txcount $i --amount 20 --multisig wallet.txt
+	./bazo-client funds --from WalletGCPCentral.txt --to WalletGCPWest.txt --txcount $i --amount 20 --multisig WalletGCPCentral.txt
 sleep 0.1
 
 done
